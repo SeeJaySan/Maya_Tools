447ffbab558dec26d73671f896ec0d6e90de63f5
SeeJaySan <99501187+SeeJaySan@users.noreply.github.com>
2025-04-13 23:31:44 -0400

    fixing parenting setup and order

--- modules/rig/character_rig_template.py
+++ modules/rig/character_rig_template.py
@@ -7,13 +7,19 @@ def CharacterTemplate():
     # Create the top-level node
     rig_node = cmds.createNode('transform', name='RIG')
 
-    # Create 'Meshes' and 'Skeleton' nodes under 'RIG'
+    # Create 'Controls" nodes under 'RIG'
+    cmds.createNode('transform', name='Controls', parent=rig_node)
+    
+    # Create 'Meshes' nodes under 'RIG'
     meshes_node = cmds.createNode('transform', name='Meshes', parent=rig_node)
-    cmds.createNode('transform', name='Skeleton', parent=rig_node)
 
     # Create 'ExportMeshes' and 'bak' nodes under 'Meshes'
     cmds.createNode('transform', name='ExportMeshes', parent=meshes_node)
     cmds.createNode('transform', name='bak', parent=meshes_node)
+    
+    # Create 'Skeleton' nodes under 'RIG'
+    cmds.createNode('transform', name='Skeleton', parent=rig_node)
+
 
     # Print the created hierarchy for verification
     print("Hierarchy created:")
